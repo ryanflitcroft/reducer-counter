c93207c41e7f2540d4215db35abd2f3285c7e487
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-11 15:46:35 -0700

    refactor state count for reducer.

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 import styles from './Counter.css';
 
 const colors = {
@@ -7,39 +7,56 @@ const colors = {
   red: 'rgb(239, 68, 68)',
 };
 
+const counter = {
+  color: colors.yellow,
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT_COUNT':
+      return { ...state, count: state.count + 1 };
+    case 'DECREMENT_COUNT':
+      return { ...state, count: state.count - 1 };
+    case 'RESET_COUNT':
+      return { ...state, count: 0 };
+  }
+};
+
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [currentColor, setCurrentColor] = useState(colors.yellow);
+  const [state, dispatch] = useReducer(reducer, counter);
+  // const [count, setCount] = useState(0);
+  // const [currentColor, setCurrentColor] = useState(colors.yellow);
 
   useEffect(() => {
-    if (count === 0) {
-      setCurrentColor(colors.yellow);
+    if (state.count === 0) {
+      // setCurrentColor(colors.yellow);
     }
 
-    if (count > 0) {
-      setCurrentColor(colors.green);
+    if (state.count > 0) {
+      // setCurrentColor(colors.green);
     }
 
-    if (count < 0) {
-      setCurrentColor(colors.red);
+    if (state.count < 0) {
+      // setCurrentColor(colors.red);
     }
-  }, [count]);
+  }, [state]);
 
   const increment = () => {
-    setCount((prevState) => prevState + 1);
+    dispatch({ type: 'INCREMENT_COUNT' });
   };
 
   const decrement = () => {
-    setCount((prevState) => prevState - 1);
+    dispatch({ type: 'DECREMENT_COUNT' });
   };
 
   const reset = () => {
-    setCount(0);
+    dispatch({ type: 'RESET_COUNT' });
   };
 
   return (
     <main className={styles.main}>
-      <h1 style={{ color: currentColor }}>{count}</h1>
+      <h1 style={{ color: state.color }}>{state.count}</h1>
       <div>
         <button
           type="button"
